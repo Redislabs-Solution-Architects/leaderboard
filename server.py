3723475b5ebdacb05ec5f11f01e6f4b8fb209770
Abhishek <srivastava07.abhishek@gmail.com>
2023-05-31 14:31:39 +0530

    more changes

--- server.py
+++ server.py
@@ -21,9 +21,8 @@ def playGame(stock):
     while time.time() < endTime:
         players = r.smembers("game:1")
         for player in players:
-            print(player)
             generatedScore = random.randint(1, 1000)
-            r.zadd("leaderboard", generatedScore, player)
+            r.zadd("leaderboard", {player: generatedScore})
             time.sleep(1)
 
 
@@ -41,7 +40,7 @@ if __name__ == '__main__':
 
 @app.route('/')
 def newplayer():
-    return render_template('add-player.html')
+    return render_template('add-player.html', location=location)
 
 @app.route('/add-player',methods = ['POST'])
 def adduser():
@@ -51,41 +50,61 @@ def adduser():
     profile = {"name": pname, "username": username, "age": age, "location": location}
     r.json().set("player:" + username, "$", profile)
     r.sadd("game:1", "player:" + username)
-    return redirect(url_for('overview', username=username))
+    return redirect(url_for('overview', username=username, location=location))
 
 @app.route('/overview')
 def overview():
     username = request.args.get('username')
     if username is None:
         username = ''
-    return render_template('overview.html', username=username)
+    return render_template('overview.html', username=username, location=location)
+
+
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template('leaderboard.html', location=location)
 
 
 @sock.route('/player/<username>')
-def price(sock, username):
+def playerMetadata(sock, username):
     print(username)
     profile = r.json().get("player:" + username)
     name = profile['name']
     location = profile['location']
-    score = 100
     while True:
+        score = r.zscore("leaderboard", "player:" + username)
         data = json.dumps(
             {"name": name, "username": username, "location": location, "score": score})
         sock.send(data)
         time.sleep(3)
 
 @sock.route('/players')
-def price(sock, username):
-    print(username)
-    profile = r.json().get("player:" + username)
-    name = profile['name']
-    location = profile['location']
-    score = 100
-    while True:
-        data = json.dumps(
-            {"name": name, "username": username, "location": location, "score": score})
+def getActivePlayers(sock):
+    players = r.sscan("game:1", cursor=0, count=2)
+    cur = players[0]
+    playerList = players[1]
+    while cur != 0:
+        for p in playerList:
+            profile = r.json().get(p)
+            data = json.dumps({"username": profile['username'], "location": profile['location']})
+            sock.send(data)
+        players = r.sscan("game:1", cursor=cur, count=2)
+        cur = players[0]
+        playerList = players[1]
+
+
+@sock.route('/game/01/leaderboard')
+def getLeaderboard(sock):
+    lb = r.zrange("leaderboard", start=0, end=9, desc=True, withscores=True)
+    i = 0
+    while i < len(lb):
+        #arg = lb[i]
+        #val = ''.join(map(str, arg))
+        val = list(lb[i])
+        data = json.dumps({"player": val[0], "score": val[1]})
         sock.send(data)
-        time.sleep(3)
+        i += 1
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5555)
